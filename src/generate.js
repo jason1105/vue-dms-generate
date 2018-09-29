cbfa6b55dd7ccaf67ef7375992e6c9923c407061
lvwei <13366824@qq.com>
2018-09-29 19:04:24 +0800

    基础功能完成

--- src/generate.js
+++ src/generate.js
@@ -1,16 +1,53 @@
 /**
- * Created by xushaoping on 17/10/11.
+ * Created by lvwei on 2018/09/29.
  */
 
-const fs = require('fs-extra');
+const fs = require('fs');
 const chalk = require('chalk');
-exports.run = function(type, name) {
-    fs.pathExists('src', (err, exists) => {
+const ejs = require('ejs')
 
-        console.log('------->');
+exports.run = function(args, name) {
+
+    let dataFilePath = findInputData(args);
+
+    // console.log("args:", args);
+    // console.log("file:", dataFilePath);
+
+    if (!dataFilePath) {
+        console.error('File not found: ', args);
+        return;
+    }
+
+    ejs.renderFile('./src/template/template.ejs', JSON.parse(fs.readFileSync(dataFilePath, {})), {}, function(err, str){
+
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(str);
+            fs.writeFile(process.cwd() + "/output.vue", str, {}, function(err){
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
 
     });
+
 };
+
+let findInputData = function(args) {
+    if (fs.existsSync(args)) {
+        return args;
+    } else {
+        if (fs.existsSync(process.cwd() + args)) {
+            return process.cwd() + args;
+        } else {
+            return null;
+        }
+    }
+};
+
+
 
 
 
